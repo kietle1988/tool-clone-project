8151e65e5e5f556345eabbd87dce9e1604558b05
Kiet Le <65524124+kietle1988@users.noreply.github.com>
2020-09-09 14:11:09 +0700

    Add tool clone project

---
+++ clone_project.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+import argparse
+import re
+import sys
+import os
+import shutil
+import subprocess
+import os.path
+
+FROM_PROJECT_ID = '3997'
+TO_PROJECT_ID = '1234'
+FROM_PROJECT = 'cc-baccarat-3997'
+TO_PROJECT = 'cc-newgame-1234'
+
+def clone_folder(fromFolder, toFolder) :
+    tool_path = os.path.dirname(os.path.realpath(__file__))
+    root_path = os.path.abspath(os.path.join(tool_path, os.pardir))
+    src_path = root_path + '/' + TO_PROJECT + '/' + fromFolder
+    dest_path = root_path + '/'+ TO_PROJECT + '/' + toFolder
+
+    # Copying the contents from Source
+    # to Destination without some
+    # specified files or directories
+    shutil.rmtree(dest_path, ignore_errors=True)
+    shutil.copytree(src_path, dest_path, ignore = shutil.ignore_patterns('*.meta', 'a'))
+
+def replaceFileContent(filename, old_string, new_string):
+    # Safely read the input filename using 'with'
+    with open(filename) as f:
+        s = f.read()
+        if old_string not in s:
+            return
+
+    # Safely write the changed content, if found in the file
+    with open(filename, 'w') as f:
+        print('Changing "{old_string}" to "{new_string}" in {filename}'.format(**locals()))
+        s = s.replace(old_string, new_string)
+        f.write(s)
+########
+# MAIN #
+########
+# # # 0.Copy to new folder, ignore .git
+tool_path = os.path.dirname(os.path.realpath(__file__))
+root_path = os.path.abspath(os.path.join(tool_path, os.pardir))
+from_project_dir = root_path + '/' + FROM_PROJECT
+to_project_dir = root_path + '/' + TO_PROJECT
+shutil.rmtree(to_project_dir, ignore_errors=True)
+shutil.copytree(from_project_dir, to_project_dir)
+shutil.rmtree(to_project_dir + '/.git', ignore_errors=True)
+
+# # 1.Copy Assets, Prefabs, Scripts
+# # 2.Remove all file meta
+clone_folder("Assets", "_Assets")
+clone_folder("Prefabs", "_Prefabs")
+clone_folder("Scripts", "_Scripts")
+
+# # 3.Rename project id
+curDir = to_project_dir + '/_Prefabs'
+subprocess.call([sys.executable, tool_path+'/rename_files.py', curDir, FROM_PROJECT_ID, TO_PROJECT_ID])
+for dirname, dirnames, filenames in os.walk(curDir):
+    for subdirname in dirnames:
+        curDir = os.path.join(dirname, subdirname)
+        subprocess.call([sys.executable, tool_path+'/rename_files.py', curDir, FROM_PROJECT_ID, TO_PROJECT_ID])
+
+curDir = to_project_dir + '/_Assets'
+subprocess.call([sys.executable, tool_path+'/rename_files.py', curDir, FROM_PROJECT_ID, TO_PROJECT_ID])
+for dirname, dirnames, filenames in os.walk(curDir):
+    for subdirname in dirnames:
+        curDir = os.path.join(dirname, subdirname)
+        subprocess.call([sys.executable, tool_path+'/rename_files.py', curDir, FROM_PROJECT_ID, TO_PROJECT_ID])
+
+curDir = to_project_dir + '/_Scripts'
+subprocess.call([sys.executable, tool_path+'/rename_files.py', curDir, FROM_PROJECT_ID, TO_PROJECT_ID])
+for dirname, dirnames, filenames in os.walk(curDir):
+    for subdirname in dirnames:
+        curDir = os.path.join(dirname, subdirname)
+        subprocess.call([sys.executable, tool_path+'/rename_files.py', curDir, FROM_PROJECT_ID, TO_PROJECT_ID])
+
+# Replace gameID in file *.js
+curDir = to_project_dir + '/_Scripts'
+for root, dirs, files in os.walk(curDir):
+    for file in files:
+        if file.endswith(".js"):
+             replaceFileContent(os.path.join(root, file), FROM_PROJECT_ID, TO_PROJECT_ID)
+
+# Rename file scene
+curDir = to_project_dir
+subprocess.call([sys.executable, tool_path+'/rename_files.py', curDir, FROM_PROJECT_ID, TO_PROJECT_ID])
+
+# Replace gameID in file R3997.fire
+curDir = to_project_dir
+for root, dirs, files in os.walk(curDir):
+    for file in files:
+        if file.endswith(".fire"):
+             replaceFileContent(os.path.join(root, file), FROM_PROJECT_ID, TO_PROJECT_ID)
---
+++ rename_files.py
@@ -0,0 +1,84 @@
+import argparse
+import os
+import re
+
+def rename_files():
+    args = parse_args()
+
+    if not os.path.isdir(args.folder):
+        print('Folder (%s) does not exist' % args.folder)
+        return
+
+#     print('arguments:')
+#     print('folder  - "%s"' % args.folder)
+#     print('pattern - "%s"' % args.pattern)
+#     print('replace - "%s"' % args.replace)
+#     print()
+
+    os.chdir(args.folder)
+    all_files = next(os.walk(args.folder))[2]
+
+    if not args.regex:
+        target_files = [file for file in all_files if args.pattern in file]
+    else:
+        target_files = {}
+        regex = re.compile(args.pattern)
+        for file in all_files:
+            matched = regex.findall(file)
+            if matched:
+                target_files[file] = list(set(matched))
+
+    if not target_files:
+#         print('No files to be renamed')
+        return
+        
+#     print('Files to be renamed (%s):' % len(target_files))
+    padding = len(max(target_files, key=len))
+
+    dest_files = []
+    replacement_string = args.replace
+    for file in target_files:
+        if not args.regex:
+            if args.append:
+                replacement_string = args.pattern + args.replace
+            elif args.prepend:
+                replacement_string = args.replace + args.pattern
+
+            altered = file.replace(args.pattern, replacement_string)
+        else:
+            for matched_pattern in target_files[file]:
+                if args.append:
+                    replacement_string = matched_pattern + args.replace
+                elif args.prepend:
+                    replacement_string = args.replace + matched_pattern
+
+                altered = file.replace(matched_pattern, replacement_string)
+
+        dest_files.append('%s --> %s' % (file.ljust(padding), altered))
+        if not args.view:
+            os.rename(file, altered)
+
+    # Dictionaries aren't sorted
+    dest_files.sort()
+    for file in dest_files:
+        print(file)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    # Required Args
+    parser.add_argument('folder', help='the folder to search through')
+    parser.add_argument('pattern', help='the pattern to look for')
+    parser.add_argument('replace', help='string to replace the pattern with')
+
+    # Optional Args
+    string_location = parser.add_mutually_exclusive_group()
+    string_location.add_argument('-a', '--append', action='store_true', help='the "replace" string will be appended to the pattern')
+    string_location.add_argument('-p', '--prepend', action='store_true', help='the "replace" string will be prepended to the pattern')
+    parser.add_argument('-r', '--regex', action='store_true', help='the pattern is a regex')
+    parser.add_argument('-v', '--view', action='store_true', help='only view the potential changes, does not rename files')
+
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    rename_files()
